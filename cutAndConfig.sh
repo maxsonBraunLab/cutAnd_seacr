b736fa4acdc06789c8af6a2b8ecbe83582d8b493
Brittany Smith <53537356+britt-smith@users.noreply.github.com>
2020-04-01 16:31:29 -0700

    Update cutAndConfig.sh

--- cutAndConfig.sh
+++ cutAndConfig.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
-### Set these THREE variables for your experiment
+### Set these FIVE variables for your experiment
 ### Make sure the project path is correct in the three scripts
 ### Change array number
 
-###TODO FILE = downsampleTodo.txt
+##1## TODO FILE
 #This file should be a list of .bam files from alignment. Include IgGs. Put in cutAnd_seacr directory
+todo=downsampleTodo.txt
 
-###METADATA FILE = metadata.txt
+##2## METADATA FILE
 #This file should have one column of samples and one column of IgG. 
 #If you are using IgGs from a different experiment use cp to copy them into the directory with other .bam files
+meta=metadata.txt
 
-##1##Choose mouse or human, place # infront of genome you aren't using
+##3## REFERENCE GENOME
+#Choose mouse or human, place # infront of genome you aren't using
 REF=/home/groups/MaxsonLab/software/ChromHMM/CHROMSIZES/hg38.txt
 #REF=/home/groups/MaxsonLab/software/ChromHMM/CHROMSIZES/mm10.txt
 
-##2##Change read number (RN) to what you want to downsample to
+##4## DOWNSAMPLING VALUE
+#Change read number (RN) to what you want to downsample to
 RN=3000000
 
-##3##Set mark
+##5## MARK
+#Make sure this mark is written the same way in your sample file names
 MARK=H3K27Ac
 
 #Executables these don't need to change
