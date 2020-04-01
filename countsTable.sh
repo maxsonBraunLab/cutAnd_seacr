77e70466cb99f2e32625312d5e0a7c5a1b795e8d
Brittany Smith <53537356+britt-smith@users.noreply.github.com>
2020-04-01 16:39:56 -0700

    rm cutAnd_seacr directory

--- countsTable.sh
+++ countsTable.sh
@@ -19,9 +19,9 @@ PROJECT=/home/groups/MaxsonLab/smithb/KASUMI_TAG_12_19
 
 source $PROJECT/cutAnd_seacr/cutAndConfig.sh
 
-IN=$PROJECT/process/cutAnd_seacr/seacr
-IN2=$PROJECT/process/cutAnd_seacr/bams
-OUT=$PROJECT/process/cutAnd_seacr/counts
+IN=$PROJECT/process/seacr
+IN2=$PROJECT/process/bams
+OUT=$PROJECT/process/counts
 mkdir -p $OUT
 
 echo "Superset total:"
