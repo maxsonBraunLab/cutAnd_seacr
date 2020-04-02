5d85bac40f1661e7a29d1d6416c60028d6d45723
bsmith-tech <curtisbr@ohsu.edu>
2020-04-02 11:54:25 -0700

    add script to merge IgG.bams

---
+++ mergeIgG.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+### PURPOSE: Filter bam files then downsample to lowest number of reads (within reason, will most likely be ~3 million)
+###                             and produce fragment bedgraphs for SEACR analysis ###
+
+#SBATCH --partition          exacloud                # partition (queue)
+#SBATCH --nodes              1                       # number of nodes
+#SBATCH --ntasks             1                       # number of "tasks" to be allocated for the job
+#SBATCH --ntasks-per-core    1                       # Max number of "tasks" per core.
+#SBATCH --cpus-per-task      4                       # Set if you know a task requires multiple processors
+#SBATCH --mem                8000                  # memory pool for each node
+#SBATCH --time               0-24:00                 # time (D-HH:MM)
+#SBATCH --output             mergeIgG_%A.out           # Standard output
+#SBATCH --error              mergeIgG_%A.err           # Standard error
+
+#Set array number and your project directory (and possibly IN directory depending on the location of your alignment files)
+
+PROJECT=/home/groups/MaxsonLab/smithb/KASUMI_TAG_12_19
+
+#########################################################
+
+source $PROJECT/4-2-20/cutAnd_seacr/cutAndConfig.sh
+
+#These don't need to change
+IN=$PROJECT/process/20_alignments
+OUT1=$PROJECT/4-2-20/process/bams
+mkdir -p $OUT1
+
+### Record slurm info
+echo "SLURM_JOBID: " $SLURM_JOBID
+
+#Sort bam files
+
+#for file in `ls $IN/*IgG.bam`; do
+#	name=`echo $file | cut -d "/" -f 9 | sed 's/.bam//g'`
+#	echo $name
+#	cmd="$SAMTOOLS sort $file -o $OUT1/$name\.sorted.bam"
+#	echo $cmd
+#	eval $cmd
+#done
+
+for file2 in `ls $OUT1/*IgG.sorted.bam`; do
+        cmd="$SAMTOOLS index $file2"
+        echo $cmd
+        eval $cmd
+done
+
+cmd3="$SAMTOOLS merge $OUT1/merge_IgG.bam $OUT1/*IgG.sorted.bam"
+echo $cmd3
+eval $cmd3 
+
+
+#cmd=$SAMTOOLS sort $i -o $OUT1/$i\.sorted.bam; done
+#cmd2=$SAMTOOLS index $OUT1/$NAME\.sorted.bam
+
+#for i in $OUT1/*IgG.sorted.bam; do NAME=${i%%.bam} | cmd="$SAMTOOLS index $OUT1/$NAME\.sorted.bam" | echo $cmd; done
+
+#for output in $cmd; do $cmd2 ; done
+
+
+
+#Merge bam files
+#cmd="SAMTOOLS merge -b $OUT1/*IgG.sorted.bam" 
