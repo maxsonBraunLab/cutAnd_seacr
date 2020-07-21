50ec039619bc36acb248ccf7174d92b67ecf3ebc
Brittany Smith <53537356+britt-smith@users.noreply.github.com>
2020-07-21 13:31:42 -0700

    changed PROJECT

--- downsample.sh
+++ downsample.sh
@@ -16,7 +16,7 @@
 
 #Set array number and your project directory (and possibly IN directory depending on the location of your alignment files)
 
-PROJECT=/home/groups/MaxsonLab/smithb/KASUMI_TAG_12_19
+PROJECT=/your/project/directory/
 
 #########################################################
 
